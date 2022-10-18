be5ad0b42a47e8db330901d46e9d6d46d5a9612c
hyoiutu <keisuke495500@gmail.com>
2022-10-19 00:39:49 +0900

    testモードを実装した

--- transport_action_cam_data.sh
+++ transport_action_cam_data.sh
@@ -1,7 +1,23 @@
 #!/bin/zsh
 
-bkups_dir="/Volumes/Elements/ActionCam"
-files=($(ls /Volumes/Untitled/MP_ROOT/100ANV01/*.MP4))
+if [ $1 = "test" ]; then
+  echo "this is test mode!"
+
+  bkups_dir="./test_destination"
+  files=($(ls ./test_source/*.MP4))
+
+  rm -rf "${bkups_dir}/*"
+elif [ $1 = "prod"]; then
+  echo "this is procution mode!"
+
+  bkups_dir="/Volumes/Elements/ActionCam"
+  files=($(ls /Volumes/Untitled/MP_ROOT/100ANV01/*.MP4))
+else
+  echo "arg must be 'test' or 'prod.'"
+  exit 1
+fi
+
+
 
 for full_path in $files; do
   creation_date=$(ffprobe -loglevel quiet ${full_path} -show_streams | grep -m1 "TAG:creation_time=" | sed -E 's/TAG:creation_time=(20[0-9][0-9]-[01][0-9]-[0-3][0-9]).*/\1/g')
@@ -17,3 +33,4 @@ for full_path in $files; do
   echo "${file_name} copy is done."
 done
 
+# date -v+9H -j -f "%Y-%m-%dT%H:%M:%S.000000Z" "2022-08-15T23:39:17.000000Z" +"%Y-%m-%d"
