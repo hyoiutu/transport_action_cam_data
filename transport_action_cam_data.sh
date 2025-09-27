410c6ba75c5c5568736fa208f2c55fd97e9bb3eb
hyoiutu <keisuke495500@gmail.com>
2025-09-27 18:22:42 +0900

    DJIの移行に対応した

--- transport_action_cam_data.sh
+++ transport_action_cam_data.sh
@@ -1,19 +1,35 @@
 #!/bin/zsh
 
-if [ $1 = "test" ]; then
-  echo "this is test mode!"
+if [ $1 = "test"  -o $1 = "test_sony" ]; then
+  echo "this is action cam test mode!"
 
   bkups_dir="./test_destination"
-  files=($(ls ./test_source/*.MP4))
+  files=($(ls ./sony_test_source/*.MP4))
+  sed_pattern='s/.*(MAH[0-9]*\.MP4)/\1/g'
 
   rm -rf "${bkups_dir}/*"
-elif [ $1 = "prod" ]; then
-  echo "this is procution mode!"
+elif [ $1 = "test_dji" ]; then
+  echo "this is dji test mode!"
+
+  bkups_dir="./test_destination"
+  files=($(ls ./dji_test_source/*.MP4))
+  sed_pattern='s/.*(DJI_[0-9]*_[0-9]{4}_D\.MP4)/\1/g'
+
+  rm -rf "${bkups_dir}/*"
+elif [ $1 = "sony" ]; then
+  echo "transporting action cam files!"
 
   bkups_dir="/Volumes/Elements/ActionCam"
   files=($(ls /Volumes/Untitled/MP_ROOT/100ANV01/*.MP4))
+  sed_pattern='s/.*(MAH[0-9]*\.MP4)/\1/g'
+elif [ $1 = "dji" ]; then
+  echo "transporting dji files!"
+
+  bkups_dir="/Volumes/Elements/DJI"
+  files=($(ls /Volumes/Untitled/DCIM/DJI_001/*.MP4))
+  sed_pattern='s/.*(DJI_[0-9]*_[0-9]{4}_D\.MP4)/\1/g'
 else
-  echo "arg must be 'test' or 'prod.'"
+  echo "arg must be 'test', 'test_sony', 'test_dji', 'sony' or 'dji'."
   exit 1
 fi
 
@@ -25,7 +41,7 @@ for full_path in $files; do
   creation_date_jst=$(date -v+9H -j -f "%Y-%m-%dT%H:%M:%S.000000Z" "${creation_datetime_gmt}" +"%Y-%m-%d")
   echo "creation date is ${creation_date_jst}"
 
-  file_name=$(echo ${full_path} | sed -E 's/.*(MAH[0-9]*\.MP4)/\1/g')
+  file_name=$(echo ${full_path} | sed -E "$sed_pattern")
   destination="${bkups_dir}/${creation_date_jst}"
   mkdir ${destination}
   echo "dir is ${destination}"
